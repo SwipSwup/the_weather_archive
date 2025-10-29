6fa6888bef08b29c1165c855679034d358be17f0
SwipSwup <david.veigel2002@gmail.com>
2025-10-29 12:04:56 +0100

    init project

---
+++ frontend/src/env.d.ts
@@ -0,0 +1,17 @@
+﻿/// <reference types="vite/client" />
+
+// Allows imports like `import something from '@/components/MyComponent.vue'`
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<object, object, any>
+    export default component
+}
+
+// Optionally define your environment variables (from .env)
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
